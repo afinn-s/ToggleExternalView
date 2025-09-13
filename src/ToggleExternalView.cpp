a2e0c096783d5cec35a5a22d9e5a05ed053c6e8b
afinn-s <afinn_s@nlesd.ca>
2025-09-13 11:49:03 -0230

    Internal camera transformations are now completely saved on view change. External zoom is saved on view change. Unable to save external view transformations for now, as the DataRefs are read-only.
    A fix will require full control of the OpenGL camera through XPLMCamera during external view

--- src/ToggleExternalView.cpp
+++ src/ToggleExternalView.cpp
@@ -2,33 +2,56 @@
 #include "XPLMDataAccess.h"
 #include "XPLMUtilities.h"
 #include "XPLMCamera.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 // Command Declarations
 
-XPLMCommandRef ToggleExternalView;
-static int CommandHandler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon);
+XPLMCommandRef toggleExternalView;
+int CommandHandler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon);
 
-XPLMCommandRef External;
-XPLMCommandRef Internal;
+XPLMCommandRef setExternal;
+XPLMCommandRef setInternal;
 
 // DataRef Declarations
 
-XPLMDataRef IsExternal;
+XPLMDataRef DR_isExternal;
+
+XPLMDataRef DR_internalRoll,
+			DR_internalHeading,
+			DR_internalPitch;
+
+XPLMDataRef DR_internalX,
+			DR_internalY,
+			DR_internalZ;
 
 // Variable Declarations
 
-XPLMCameraPosition_t PreviousCockpitPosition;
+XPLMCameraPosition_t previousExternalPosition;
+XPLMCameraPosition_t previousInternalPosition;
+
+// Variable Definitions
+
+struct Position
+{
+	float roll, heading, pitch;
+	float x, y, z;
+};
+
+bool externalPositionSaved{false};
+bool internalPositionSaved{false};
+
+Position external;
+Position internal;
 
 // Function Definitions
 
-static int SetCockpitPosition (XPLMCameraPosition_t *outCameraPosition, int inIsLosingControl, void *inRefcon)
+int setPosition (XPLMCameraPosition_t *outCameraPosition, int inIsLosingControl, void *inRefcon)
 {
 	if (!inIsLosingControl)
 	{
-		*outCameraPosition = PreviousCockpitPosition;
+		XPLMGetDatai(DR_isExternal) ?
+		*outCameraPosition = previousExternalPosition :
+		*outCameraPosition = previousInternalPosition;
+
 		XPLMDontControlCamera();
 		return 1;
 	}
@@ -45,18 +68,25 @@ PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc)
 	strcpy(outDesc, "Toggle the cockpit view.");
 
 	// Command Definitions
-	ToggleExternalView = XPLMFindCommand("sim/view/3d_cockpit_toggle");
+	toggleExternalView = XPLMFindCommand("sim/view/3d_cockpit_toggle");
 
-	External = XPLMFindCommand("sim/view/chase");
-	Internal = XPLMFindCommand("sim/view/3d_cockpit_cmnd_look");
+	setExternal = XPLMFindCommand("sim/view/chase");
+	setInternal = XPLMFindCommand("sim/view/3d_cockpit_cmnd_look");
 
 	// Assign the command to a usuable function (handler)
-	XPLMRegisterCommandHandler(ToggleExternalView, CommandHandler, 1, nullptr);
+	XPLMRegisterCommandHandler(toggleExternalView, CommandHandler, 1, nullptr);
 
 	// DataRef Definitions
 
-	IsExternal = XPLMFindDataRef("sim/graphics/view/view_is_external");
+	DR_isExternal = XPLMFindDataRef("sim/graphics/view/view_is_external");
 
+	DR_internalRoll = XPLMFindDataRef("sim/graphics/view/pilots_head_phi");
+	DR_internalHeading = XPLMFindDataRef("sim/graphics/view/pilots_head_psi");
+	DR_internalPitch = XPLMFindDataRef("sim/graphics/view/pilots_head_the");
+
+	DR_internalX = XPLMFindDataRef("sim/graphics/view/pilots_head_x");
+	DR_internalY = XPLMFindDataRef("sim/graphics/view/pilots_head_y");
+	DR_internalZ = XPLMFindDataRef("sim/graphics/view/pilots_head_z");
 	return 1;
 }
 
@@ -65,8 +95,8 @@ PLUGIN_API int XPluginEnable(void)
 { return 1; }
 
 // Unused required empty functions
-PLUGIN_API void XPluginDisable(void){}
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFromWho, long inMessage, void *inParam){}
+PLUGIN_API void XPluginDisable(void);
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFromWho, long inMessage, void *inParam);
 
 // CommandHandler is called on execution of the 3D cockpit toggle
 int CommandHandler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inRefcon)
@@ -74,15 +104,58 @@ int CommandHandler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inR
 	// Code is executed on button up.
 	if (inPhase == xplm_CommandEnd)
 	{
-        if (XPLMGetDatai(IsExternal))
+        if (XPLMGetDatai(DR_isExternal))
         {
-            XPLMCommandOnce(Internal);
-			XPLMControlCamera(xplm_ControlCameraUntilViewChanges, SetCockpitPosition, nullptr);
+			// Save the external OpenGL camera transformations
+			XPLMReadCameraPosition(&previousExternalPosition);
+			externalPositionSaved = true;
+
+			//----
+            XPLMCommandOnce(setInternal);
+			//----
+
+			// OpenGL camera must temporaly be controlled for zoom to be set
+			if (internalPositionSaved)
+				XPLMControlCamera(xplm_ControlCameraUntilViewChanges, setPosition, nullptr);
+			
+			// Internal DataRef camera transformations are set
+			XPLMSetDataf(DR_internalRoll, internal.roll);
+			XPLMSetDataf(DR_internalHeading, internal.heading);
+			XPLMSetDataf(DR_internalPitch, internal.pitch);
+			
+			XPLMSetDataf(DR_internalX, internal.x);
+			XPLMSetDataf(DR_internalY, internal.y);
+			XPLMSetDataf(DR_internalZ, internal.z);
         }
         else
         {
-			XPLMReadCameraPosition(&PreviousCockpitPosition);
-			XPLMCommandOnce(External);
+			// Save the internal OpenGL camera transformations
+			XPLMReadCameraPosition(&previousInternalPosition);
+			internalPositionSaved = true;
+			
+			// Save the internal DataRef camera transformations
+			internal.roll = XPLMGetDataf(DR_internalRoll);
+			internal.heading = XPLMGetDataf(DR_internalHeading);
+			internal.pitch = XPLMGetDataf(DR_internalPitch);
+
+			internal.x = XPLMGetDataf(DR_internalX);
+			internal.y = XPLMGetDataf(DR_internalY);
+			internal.z = XPLMGetDataf(DR_internalZ);
+
+			//----
+			XPLMCommandOnce(setExternal);
+			//----
+
+			// OpenGL camera must temporaly be controlled for zoom to be set
+			if (externalPositionSaved)
+				XPLMControlCamera(xplm_ControlCameraUntilViewChanges, setPosition, nullptr);
+			
+			/* NOTE:
+			   External DataRef camera transformations cannot currently
+			   be set, as the DataRefs are read only. The only solutions
+			   are modifying the values in memory or taking full control
+			   and managment of the OpenGL camera when set to external.
+			*/
         }
 	}
 	// Returning 0 disables further processing by X-Plane, therefore
@@ -90,6 +163,6 @@ int CommandHandler(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void *inR
 	return 0;
 }
 
-// Unregisters the plugin when it is terminated by X-Plane
+// Unregisters the CommandHanlder when it is terminated by X-Plane
 PLUGIN_API void XPluginStop(void)
-{ XPLMUnregisterCommandHandler(ToggleExternalView, CommandHandler, 0, 0); }
+{ XPLMUnregisterCommandHandler(toggleExternalView, CommandHandler, 0, 0); }
